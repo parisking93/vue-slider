a2dedd21a6c5f2a98104d8c262d09505cda425a5
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-22 17:04:55 +0200

    add cambio foto ogni 3 secondi automaticamente

--- js/script.js
+++ js/script.js
@@ -6,18 +6,21 @@ const app = new Vue(
             counter : 0,
             images : ["https://picsum.photos/id/230/500/300","https://picsum.photos/id/231/500/300","https://picsum.photos/id/237/500/300","https://picsum.photos/id/234/500/300","https://picsum.photos/id/236/500/300"]
         },
-         methods : {
-             next : function() {
+        created : function() {
+            return setInterval(this.next, 3000);
+        },
+        methods : {
+            next : function() {
                  (this.counter == this.images.length - 1) ? this.counter = 0 : this.counter++;
                  console.log(this.counter,this.images.length - 1);
              },
-             prev : function() {
+            prev : function() {
                 (this.counter == 0) ? this.counter = this.images.length -1 : this.counter--;
                 console.log(this.counter,this.images.length - 1);
             },
             cambia : function(indice) {
                 this.counter = indice
             }
-         }
+        }
     }
 );
