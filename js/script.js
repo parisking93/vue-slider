7235f229ac63d3c5ef093470a4be5a455a6f9a32
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-23 16:40:58 +0200

    add button autoplay on and off e space key per autoplay

--- js/script.js
+++ js/script.js
@@ -4,22 +4,43 @@ const app = new Vue(
 
         data : {
             counter : 0,
-            images : ["https://picsum.photos/id/230/500/300","https://picsum.photos/id/231/500/300","https://picsum.photos/id/237/500/300","https://picsum.photos/id/234/500/300","https://picsum.photos/id/236/500/300"]
+            images : ["https://picsum.photos/id/230/500/300","https://picsum.photos/id/231/500/300","https://picsum.photos/id/237/500/300","https://picsum.photos/id/234/500/300","https://picsum.photos/id/236/500/300"],
+            autoplay : "",
+            intervallo : '',
+            check : ''
         },
-        created : function() {
-            return setInterval(this.next, 3000);
+        mounted(){
+            this.autoplay = true;
+            this.intervallo = setInterval(this.next, 3000);
+
+            document.addEventListener('keyup', (event)=>{
+                if (event.key == ' ' && this.autoplay == true) {
+                    this.stop();
+                    this.autoplay =false;
+                } else if (event.key == ' ' && this.autoplay == false) {
+                    this.set();
+                    this.autoplay =true;
+                }
+            });
         },
         methods : {
             next : function() {
                  (this.counter == this.images.length - 1) ? this.counter = 0 : this.counter++;
-                 console.log(this.counter,this.images.length - 1);
              },
             prev : function() {
                 (this.counter == 0) ? this.counter = this.images.length -1 : this.counter--;
-                console.log(this.counter,this.images.length - 1);
+
             },
             cambia : function(indice) {
                 this.counter = indice
+            },
+            set : function() {
+                this.autoplay = true;
+                this.intervallo = setInterval(this.next, 3000);;
+            },
+            stop : function() {
+               this.autoplay = false;
+               this.intervallo = clearInterval(this.intervallo);
             }
         }
     }
